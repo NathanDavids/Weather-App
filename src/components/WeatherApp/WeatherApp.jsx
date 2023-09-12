f2beada13f21308cc280a4fc600d3b5be516d98b
NathanDavids <129671319+NathanDavids@users.noreply.github.com>
2023-09-12 10:02:47 +0200

    Added the containers to contain the Weather Info

--- src/components/WeatherApp/WeatherApp.jsx
+++ src/components/WeatherApp/WeatherApp.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import '../WeatherApp/WeatherApp.css'
 import search_icon from '../../assets/search.png'
 // import clear_icon from '../../assets/clear.png'
-// import cloud_icon from '../../assets/cloud.png'
+import cloud_icon from '../../assets/cloud.png'
 // import drizzle_icon from '../../assets/drizzle.png'
 // import rain_icon from '../../assets/rain.png'
 // import snow_icon from '../../assets/snow.png'
-// import wind_icon from '../../assets/wind.png'
-// import humidity_icon from '../../assets/humidity.png'
+import wind_icon from '../../assets/wind.png'
+import humidity_icon from '../../assets/humidity.png'
 
 function WeatherApp() {
   return (
@@ -19,7 +19,28 @@ function WeatherApp() {
                 <div className='search-icon'>
                     <img src={search_icon} alt="" />
                 </div>
-            </div>    
+            </div>   
+            <div className='weather-image'>
+              <img src={cloud_icon} alt="" />
+            </div> 
+            <div className='weather-temp'> 24°C </div>
+            <div className='weather-location'> London </div>
+            <div className="data-container">
+              <div className="element">
+                <img src={humidity_icon} alt="" />
+                <div className="data">
+                  <div className="humidity-percent"> 64% </div>
+                  <div className='text'> Humiduty </div>
+                </div>
+              </div>
+              <div className="element">
+                <img src={wind_icon} alt="" />
+                <div className="data">
+                  <div className="humidity-percent"> 18 km/h </div>
+                  <div className='text'> Wind Speed </div>
+                </div>
+              </div>
+            </div>
         </div> 
     </section>
     </>
